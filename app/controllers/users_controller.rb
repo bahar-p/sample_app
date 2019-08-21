c5d8aa293eb758cb3d8be2c15dcf6c8ec3ec4ae8
Bahar Pourazar <bahar.pourazar@shopify.com>
2019-08-21 10:10:40 -0400

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
+      log_in(@user)
       redirect_to @user
     else
       render 'new'
@@ -22,7 +23,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-
   end
 
 end
